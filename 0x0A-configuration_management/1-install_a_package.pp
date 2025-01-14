67a8fa3b76a6dfc7bbba054bb5dcdb8a1c3e923f
lilprof01 <adewumime@gmail.com>
2025-01-14 06:16:32 -0800

    more fixes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # Using Puppet, install flask from pip3.
 
+package { 'Python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => package['flask'],
 }
