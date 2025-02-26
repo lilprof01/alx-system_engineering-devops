ba2d1e08b7d9948c9e4155bb9e503f7229b629f5
lilprof01 <adewumime@gmail.com>
2025-02-26 03:14:16 -0800

    puppet script for debugging WP site

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# automated puppet script to figure out why Apache is returning a 500 error
+
+exec {
+'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
