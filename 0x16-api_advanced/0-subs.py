57d4c35de3fe1acf6d5925a46c7a198dd9494587
lilprof01 <adewumime@gmail.com>
2025-02-27 03:08:16 -0800

    tak 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module queries the Reddit API and returns the number of subscribersfor a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function queries the Reddit API"""
+    headers = {
+        "User-Agent": "0x16. API_advanced-e_kiminza"
+        }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    sub = response.json().get("data").get("subscribers")
+    return sub
